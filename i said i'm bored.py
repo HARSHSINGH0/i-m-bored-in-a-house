811ef9545fbdc5dc03e7a6581fb96086227cf522
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2020-04-02 13:44:43 +0530

    Create i said i'm bored.py

---
+++ i said i'm bored.py
@@ -0,0 +1,21 @@
+"""
+h
+ha
+har
+hars
+harsh
+hars
+har
+ha
+h
+"""
+word="quarantined"
+for i in range(100):
+	a=1
+	for i in range(len(word)):
+		print(word[:a])
+		a+=1
+	b=0
+	for i in range(len(word)):
+		print(word[b:])
+		b+=1
